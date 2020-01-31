b159a84020b4fe76185b6cb045623d79706b922e
Visal Rajapakse <visalrajapakse@gmail.com>
2020-01-31 09:34:18 +0530

    complete dice game

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let dices = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
 
+
+    @IBOutlet weak var diceImageOne: UIImageView!
+    @IBOutlet weak var diceImageTwo: UIImageView!
+    @IBAction func rollButton(_ sender: Any) {
+        
+        let randomDiceOne = Int.random(in: 0...5)
+        let randomDiceTwo = Int.random(in: 0...5)
+        
+        diceImageOne.image = UIImage(named: dices[randomDiceOne])
+        diceImageTwo.image = UIImage(named: dices[randomDiceTwo])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
